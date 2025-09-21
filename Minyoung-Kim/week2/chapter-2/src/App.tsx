f00cb0cd6e0d584b2d683bc995f1e1c39f7a38a6
minzero31 <77minyoung77@gmail.com>
2025-09-22 02:01:49 +0900

    [Feat(마인/김민영)] : useState 심화 실습, 함수 분리

--- Minyoung-Kim/week2/chapter-2/src/App.tsx
+++ Minyoung-Kim/week2/chapter-2/src/App.tsx
@@ -1,16 +1,19 @@
-// useState 기초 실습 : 버튼 클릭 시 마다 숫자 증가 (0 ->)
-
 import './App.css'
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0) // 초기값을 0으로 설정
+  const [count, setCount] = useState(0)
+
+  {/* onClick 시 setCount 하는 함수를 분리해서 작성하기 */}
+
+  const handleIncreaseNumber = () => {
+    setCount(count + 1)
+  }
+
   return (
      <>
       <h1>{count}</h1>
-      {/* onClick 이벤트 핸들러를 사용함 */}
-      {/* 바꾸고 싶은 두 번째 상태 값을 넣어주기(setCount로 1씩 증가하는 숫자) */}
-      <button onClick={() => setCount(count + 1)}>숫자 증가</button>
+      <button onClick={handleIncreaseNumber}>숫자 증가</button>
      </>
   )
 }
