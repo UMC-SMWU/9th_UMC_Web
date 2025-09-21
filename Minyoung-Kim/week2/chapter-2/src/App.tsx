ecd95d0099f046e79a1c6d51e1a9d5f323b65f89
minzero31 <77minyoung77@gmail.com>
2025-09-22 01:48:23 +0900

    [Feat(마인/김민영)] : 컴포넌트 분리 및 타입 지정 실습

--- Minyoung-Kim/week2/chapter-2/src/App.tsx
+++ Minyoung-Kim/week2/chapter-2/src/App.tsx
@@ -1,11 +1,38 @@
-import './App.css'
+import './App.css';
+import List, { type Tech } from './components/List';
 
 function App() {
+  const nickname = '매튜';
+  const sweetPotato = '고구마';
+  const array = ['REACT', 
+    'NEXT', 
+    'VUE', 
+    'SVELTE', 
+    'ANGULAR', 
+    'REACT-NATIVE'
+  ] as const;
+  
   return (
     <>
-      <h1>Hello React</h1>
+      <strong className='school'>상명대학교</strong>
+      <p
+        style={{
+          color: 'purple',
+          fontWeight: 'bold',
+          fontSize: '3rem',
+        }}
+      >
+        {nickname}/김용민
+      </p>
+      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
+      <ul>
+        {array.map((yaho, idx) => (
+	        // as를 통한 타입 단언
+          <List key={idx} tech={yaho as Tech} />
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
