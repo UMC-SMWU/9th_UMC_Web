6a21fb491fcb2c233cf4f28eb81832715c34c421
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-09-23 02:06:56 +0900

    [Feat(간/김가인)] 동작 기능 추가

---
+++ gain-0525/week2/src/App.tsx
@@ -0,0 +1,15 @@
+import type { ReactElement } from 'react'
+import './App.css'
+import Todo from './components/Todo'
+
+function App() : ReactElement {
+  
+
+  return (
+    <>
+      <Todo />
+    </>
+  )
+}
+
+export default App
