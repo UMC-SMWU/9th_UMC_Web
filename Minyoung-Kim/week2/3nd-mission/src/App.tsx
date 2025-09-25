03a0f8fe11460fd2808248c1abe55be99bf4e21d
minzero31 <77minyoung77@gmail.com>
2025-09-26 00:42:25 +0900

    [Feat(마인/김민영)] : useContext를 활용하여 다크/라이트 모드 구현

---
+++ Minyoung-Kim/week2/3nd-mission/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import ContextPage from './useContext/ContextPage';
+
+export default function App() {
+  return (
+    <>
+      <ContextPage/>
+    </>
+  );
+}
---
+++ Minyoung-Kim/week2/3nd-mission/src/useContext/Navbar.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+import ThemeToggleButton from './ThemeToggleButton';
+import { useTheme, THEME } from "./context/ThemeProvider";
+
+export default function Navbar() {
+    const { theme } = useTheme();
+    const isLightMode = theme === THEME.LIGHT;
+
+    return (
+      <nav
+        className={clsx(
+            'p-4 w-full flex justify-end',
+            isLightMode ? 'bg-white' : 'bg-gray-800'
+        )}
+      >
+        <ThemeToggleButton />
+      </nav>
+    );
+}
