e1fd3a3bc76cd583767d653ab6c6978d17dd8b2a
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-10-28 11:44:09 +0900

    [fix(간/김가인)] 오류 수정

--- gain-0525/week5/5-2/src/apis/auth.ts
+++ gain-0525/week5/5-2/src/apis/auth.ts
@@ -11,11 +11,7 @@ export const postSignin = async (body: RequestSigninDto): Promise<ResponseSignin
   return data;
 };
 
-export const getMyInfo = async (): Promise<ResponseMyInfoDto> => {
-  const { data } = await axiosInstance.get("v1/users/me");
-  return data;
-<<<<<<< HEAD
-};
-=======
-};
->>>>>>> 2f3834b ([Fix(간/김가인)] RefreshToken 추가)
+export const getMyInfo = async() : Promise<ResponseMyInfoDto> => {
+    const { data } = await axiosInstance.get("v1/users/me");
+    return data;
+}
