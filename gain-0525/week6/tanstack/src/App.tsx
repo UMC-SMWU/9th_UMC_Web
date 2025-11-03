5c352af5a1bc1f44fc2cddb2b302498733df8e1b
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-11-03 14:54:19 +0900

    [Fix(간/김가인)] QueryStack 사용

--- gain-0525/week6/tanstack/src/App.tsx
+++ gain-0525/week6/tanstack/src/App.tsx
@@ -1,42 +1,17 @@
-import { type ReactElement } from 'react';
+import type { ReactElement } from 'react';
 import './App.css';
-import { useCustomFetch } from './hooks/useCustomFetch';
+import { WelcomeData } from './components/UserDataDisplay';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
-const STEAL_TIME = 5 & 60 * 1_000; // 5minutes
+const queryClient = new QueryClient();
 
-//로컬 스토리지에 저장할 데이터의 구조
-interface CacheEntry<T> {
-  data: T;
-  lastFetched:number;
-}
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-
-function App() : ReactElement {
-  const {data, isPending, isError } = useCustomFetch<User>(
-    'https://jsonplaceholder.typicode.com/users/1'
-  );
-  console.log(isPending); 
-
-  if (isError) {
-    return <div> 고치기 </div>
-  }
-
-  if (isPending) {
-    return <div>Loading...</div>
-  }
-
+export function App() : ReactElement {
   return (
-    <>
-      <h1>Tanstack Query</h1>
-      {data?.name}
-    </>
-
+    <QueryClientProvider client={queryClient}>
+      <WelcomeData />
+    </QueryClientProvider>
   )
+  
 }
 
 export default App;
--- gain-0525/week6/tanstack/src/hooks/useCustomFetch.ts
+++ gain-0525/week6/tanstack/src/hooks/useCustomFetch.ts
@@ -1,74 +1,30 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useQuery, type UseQueryResult } from '@tanstack/react-query';
 
-const STALE_TIME = 0.3 * 60 * 1_000; // 5minutes
+export const useCustomFetch = <T>(url:string) : UseQueryResult<NoInfer<T>,
+Error> => {
+  return useQuery({
+    queryKey: [url],
 
-//로컬 스토리지에 저장할 데이터의 구조
-interface CacheEntry<T> {
-  data: T;
-  lastFetched:number; //마지막으로 데이터를 가져온 시점 타임스탬프
+    queryFn: async({ signal }) : Promise<T> => {
+      const response = await fetch(url, { signal });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      return response.json() as Promise<T>
+    },
+     retry: 10,
+     // 지수 백오프 전략
+     retryDelay: (attemptIndex) : number => {
+      return Math.min(1000 * Math.pow(2, attemptIndex), 30_000);
+     },
+
+     staleTime: 5 * 60 * 1_000,
+
+     //쿼리가 사용되지 않은 채로 10분이 지나면 캐시에서 제거한다.
+     gcTime: 10 * 60 * 1_000,
+  })
+
 }
 
-export const useCustomFetch = <T>(url: string): { data: T | null; isPending: boolean; isError:boolean } => {
-  const [data, setData] = useState<T | null>(null);
-  const [isPending, setIsPending] = useState<boolean>(false);
-  const [isError, setIsError] = useState<boolean>(false);
-
-  const storageKey = useMemo(():string => url, [url])
-
-  useEffect((): void => {
-    setIsError(false);
-
-    const fetchData = async (): Promise<void> => {
-      const currentTime = new Date().getTime();
-      const cachedItem = localStorage.getItem(storageKey);
-
-      //캐시 데이터 확인, 신선도 검증
-      if (cachedItem) {
-        try {
-          const cachedData: CacheEntry<T> = JSON.parse(cachedItem);
-          if (currentTime - cachedData.lastFetched < STALE_TIME) {
-            setData(cachedData.data);
-            setIsPending(false);
-            console.log('캐시된 데이터 사용', url);
-            return;
-          }
-
-          //캐시가 만료된 경우
-          setData(cachedData.data);
-          console.log('만료된 캐시 데이터 사용', url);
-        } catch {
-          localStorage.removeItem(storageKey);
-          console.warn('캐시 에러: 캐시 삭제함', url);
-        }
-
-      }
-
-      setIsPending(true);
-      try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-
-        const newData = (await response.json()) as T;
-        setData(newData);
-
-        const newCacheEntry: CacheEntry<T> = {
-          data: newData,
-          lastFetched: new Date().getTime(),
-        }
-        
-        localStorage.setItem(storageKey, JSON.stringify(newCacheEntry));
-      } catch (error) {
-        setIsError(true);
-        console.log(error);
-      } finally {
-        setIsPending(false);
-      }
-    };
-    fetchData();
-  },[url, storageKey]);
-
-  return { data, isPending, isError };
-};
