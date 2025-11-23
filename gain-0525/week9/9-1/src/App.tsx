c30e96b599dd0cf448ce6229652e704f8ddbdb6f
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-11-23 23:21:38 +0900

    [Chore(간/김가인)] 기초 세팅 및 UI 제작

---
+++ gain-0525/week9/9-1/src/App.tsx
@@ -0,0 +1,14 @@
+import CartList from "./components/CartList"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <CartList/>
+
+    </>
+  )
+}
+
+export default App
