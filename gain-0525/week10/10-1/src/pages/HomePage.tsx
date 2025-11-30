1aef3b09eee4ca30dcdcea56c6a1311848d25cf5
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-12-01 03:36:24 +0900

    [Feat(간/김가인)] 최적화

---
+++ gain-0525/week10/10-1/src/pages/HomePage.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useMemo, useState } from "react";
+import MovieFilter from "../components/MovieFilter";
+import MovieList from "../components/MovieList";
+import useFetch from "../hooks/useFetch";
+import { type MovieFilters, type MovieResponse } from "../types/movie";
+
+export default function HomePage() {
+    const [filters, setFilters] = useState<MovieFilters>({
+        query: "어벤져스",
+        include_adult: false,
+        language: "ko-KR",
+    });
+
+    const axiosRequestConfig = useMemo(
+        () : { params: MovieFilters }=> ({
+        params: filters,
+    }),
+    [filters],
+);
+    const {data, error, isLoading} = 
+    useFetch<MovieResponse>(
+        "/search/movie", 
+        axiosRequestConfig,
+    );
+
+    const handleMovieFilters = useCallback(
+    (filters: MovieFilters) => {
+        setFilters(filters);
+    },
+    [setFilters],
+);
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return (
+        <div className="container mx-auto px-4">
+            <MovieFilter onChange={handleMovieFilters}/>
+            {isLoading ? (
+                <div>로딩 중 입니다...</div>
+            ) : (
+                <MovieList movies = {data?.results || []} />
+             )}
+        </div>
+    );
+};
+
+// 검색 필터
+// 영화 무비
